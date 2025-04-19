90ed9a17ee053fa4a5e282498e6d3769710e7da8
P-Point-all <slisse.paul@gmail.com>
2025-04-19 14:51:43 +0200

    Implementation of RW, need to be tested

--- covid.py
+++ covid.py
@@ -7,24 +7,23 @@ import matplotlib.pyplot as plt
 import csv
 from toy_example import BuildD
 
-def Buildsh(T):
-    sh = np.zeros(2*T)
-    sh[0] = (a-2*b)/4
-    sh[1] = b/4
-    return sh
+def Buildbarsh(T,a,b):
+    barsh = np.zeros(2*T)
+    barsh[0] = (a-2*b)/4
+    barsh[1] = b/4
+    return barsh
 
 #Calcul de la log-densité pour une seule valeur, pour l'appliquer à un tableau , on pourra utiliser apply_along_axis
-def log_pi(theta, phi,Z, lambda_R, D, sh, lambda_O,c,C):
+def log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c,C):
     
-    sh, _ = np.split(sh, 2)
+    shR, shO = np.split(barsh, 2)
     R, O = np.split(theta, 2) 
-    log_pi_val = -np.sum(R*phi + O - Z*np.log(R*phi + c*O + 1e-10)) - lambda_R*np.linalg.norm(D@R + sh[:T], ord = 1) - lambda_O*np.linalg.norm(c@O+sh[T:], ord = 1)
+    log_pi_val = -np.sum(R*phi + O - Z*np.log(R*phi + c*O + 1e-10)) - lambda_R*np.linalg.norm(D@R + shR, ord = 1) - lambda_O*np.linalg.norm(C@O+shO, ord = 1)
     
     return log_pi_val
 
-def PD3S(file_Z, file_phi, niter = 1e5):
-    
-    # lambda_O, lambda_R, c = phi?, nom du fichier?
+
+def load_data(file_Z,file_phi):
     Z = []
     with open(file_Z) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
@@ -35,24 +34,27 @@ def PD3S(file_Z, file_phi, niter = 1e5):
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
             phi.append(float(row[0]))
+    return np.array(Z),np.array(phi)
 
+
+def PD3S(Z, phi, niter = 1e5):
+    
+    # lambda_O, lambda_R, c = phi?, nom du fichier?
+    
     a = 0.73597
     b = 0.73227
     #Z = Z[2:]
     #phi = phi[2:]
 
     T = len(Z)
-    c = np.array(phi) #?
-    Z = np.array(Z)
+    c = phi #?
     
     lambda_O = 0.05
     lambda_R = 3.5*np.std(Z)
     D = BuildD(T)
     C = np.diag(c)
     A = np.block([[D, np.zeros((T,T))],[np.zeros((T,T)), (lambda_O/lambda_R)*C]])
-    sh = np.zeros(2*T)
-    sh[0] = (a-2*b)/4
-    sh[1] = b/4
+    barsh = Buildbarsh(T,a,b)
     B = np.block([np.diag(phi), C])
 
     mu = 1e-5 #?
@@ -71,7 +73,7 @@ def PD3S(file_Z, file_phi, niter = 1e5):
 
     for k in range(niter):
         tau = s + delta*A@theta + delta*A@M
-        s = tau + delta*sh - np.maximum(np.abs(tau + delta*sh) - lambda_R, np.zeros(2*T))*np.sign(tau + delta*sh)
+        s = tau + delta*barsh - np.maximum(np.abs(tau + delta*barsh) - lambda_R, np.zeros(2*T))*np.sign(tau + delta*barsh)
         z = theta - gamma*(A.T)@s
         u = B@z - gamma*np.diagonal(B@(B.T)) + np.sqrt((B@z - gamma*np.diagonal(B@(B.T)))**2 + 4*gamma*Z*np.diagonal(B@(B.T)))
         M = - theta
@@ -81,23 +83,27 @@ def PD3S(file_Z, file_phi, niter = 1e5):
         
     return theta
 
-def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
+def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
     
-    
+    a = 0.73597
+    b = 0.73227
     gamma = 0.001
     accept_final = 0.24
     D=BuildD(T)
-    sh = Buildsh(T)
+    barsh = Buildbarsh(T,a,b)
     c = np.array(phi)
     C = np.diag(c)
     theta = np.concatenate((np.ones(T),np.zeros(T))) #Starting point
+    A = np.block([[D, np.zeros((T,T))],[np.zeros((T,T)), (lambda_O/lambda_R)*C]])
+    is_image = method in ["image", "subdiff_image"]
+    is_source = method in ["source", "subdiff_source"]
     
-    # Covariance matrix C
+    # Covariance matrix Cov
     if is_image:
-        D_1 = npl.solve(D, np.identity(T))
-        C = np.block([[D_1@D_1.T, np.zeros((T,T))],[np.zeros((T,T)), D_1@D_1.T]])
+        A_1 = npl.solve(A, np.identity(2*T))
+        Cov = A_1@A_1.T
     elif is_source:
-        C = np.identity(2*T)
+        Cov = np.identity(2*T)
     else:
         raise Exception("method must be either 'source' or 'image' ")
 
@@ -111,7 +117,6 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
     theta_tab = np.empty((int(niter+1), 2*T))
     theta_tab[0,:]=theta
     theta_tilde_tab = np.empty((int(niter+1), 2*T))
-    theta_tilde_tab[0,:]=D@theta
     converge=0
 
     # for plotting
@@ -120,13 +125,21 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
     accepts = []
     gammas.append(gamma)
     accepts.append(0)
-
+    
+    plt.ion()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    ax1.set_title("Évolution de R")
+    ax2.set_title("Évolution de O")
+    
+    #Computation of the MAP for comparison
+    MAPR,MAPO=np.split(MAP,2)
+    
     # burn-in loop
     for i in range(int(niter/2)):
 
         mu = theta
-        candidate = npr.multivariate_normal(mu, gamma*C)
-        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, sh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, sh, lambda_O,c ,C )
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )
             
         if log_alpha >=0 :
             theta = candidate
@@ -136,8 +149,7 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
                 theta = candidate
                 acceptance_cnt += 1
                 
-        theta_tab[i+1,:]=theta
-        theta_tilde_tab[i+1,:]=D@theta    
+        theta_tab[i+1,:]=theta   
         
         # burn-in
         if ((i+1) % 1000) == 0:
@@ -151,11 +163,24 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
                 wait_conv = not burn_in
             elif wait_conv:
                 converge += 1
-                wait_conv = converge < 1e-4 * niter
+                wait_conv = converge < 2e-4 * niter
                 if not(wait_conv):
                     end_burn_in=i
                     break
-    
+        if ((i+1) % 50000) == 0:
+            R,O=np.split(theta,2)
+            ax1.clear()
+            ax2.clear()
+            ax1.plot(R,label=f"Simulation of R at iter {i}")
+            ax1.plot(MAPR,label="MAP R")
+            ax2.plot(O,label=f"Simulation of O at iter {i}")
+            ax2.plot(MAPR,label="MAP O")
+            ax1.set_title("Évolution de R")
+            ax2.set_title("Évolution de O")
+            ax1.legend()
+            ax2.legend()
+        
+            plt.pause(0.1) 
     if(wait_conv):
         end_burn_in=int(niter/2)
     
@@ -164,8 +189,8 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
     ## convergence loop
     for i in range(end_burn_in,int(niter)):
         mu = theta
-        candidate = npr.multivariate_normal(mu, gamma*C)
-        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, sh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, sh, lambda_O,c ,C )
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )
         if log_alpha >=0 :
             theta = candidate
         else:
@@ -173,6 +198,20 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,niter=1e5,method="source"):
                 theta = candidate
             
         theta_tab[i+1,:] = theta
-        theta_tilde_tab[i+1,:] = D @ theta
+        if ((i+1) % 50000) == 0:
+            R,O=np.split(theta,2)
+            ax1.clear()
+            ax2.clear()
+            ax1.plot(R,label=f"Simulation of R at iter {i}")
+            ax1.plot(MAPR,label="MAP R")
+            ax2.plot(O,label=f"Simulation of O at iter {i}")
+            ax2.plot(MAPO,label="MAP O")
+            ax1.legend()
+            ax2.legend()
         
+            plt.pause(0.1) 
+            
+    plt.ioff()
+    plt.show()
+    theta_tilde_tab = np.einsum('ij,kj->ki', A, theta_tab)
     return theta_tab,theta_tilde_tab, accepts, gammas,end_burn_in
