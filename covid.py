f08ba69d2f3c63fdbcbb28611ca450288f17b758
P-Point-all <slisse.paul@gmail.com>
2025-04-29 15:18:25 +0200

    Try to implement subdiff on covid data, prepared all the plots, just need to work now (and simulate)

--- covid.py
+++ covid.py
@@ -22,6 +22,14 @@ def log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c,C):
     
     return log_pi_val
 
+def log_pi_with_fixed_O(theta_R, MAPO,phi,Z, lambda_R, D, barsh, lambda_O,c,C):
+    theta_complete = np.concatenate([theta_R, MAPO])
+    return log_pi(theta_complete, phi, Z, lambda_R, D, barsh, lambda_O, c, C)
+
+def log_pi_with_fixed_R(theta_O, MAPR,phi,Z, lambda_R, D, barsh, lambda_O,c,C):
+    theta_complete = np.concatenate([MAPR, theta_O])
+    return log_pi(theta_complete, phi, Z, lambda_R, D, barsh, lambda_O, c, C)
+    
 def load_data(file_Z,file_phi):
     Z = []
     with open(file_Z) as csvDataFile:
@@ -35,7 +43,18 @@ def load_data(file_Z,file_phi):
             phi.append(float(row[0]))
     return np.array(Z),np.array(phi)
 
+def CalculSubdiff(Z,phi,theta,A,c,lambda_R,lambda_O,barsh,gamma,Cov):
+    R,O=np.split(theta,2)
+    gradfR=phi-Z*phi/(R*phi+c*O)
+    gradfO=c-Z*phi/(R*c+c*O)
+    subgradg=(A.T)@np.sign(A@theta+barsh)
+    gradf=np.concatenate((gradfR,gradfO))
+    return theta - (gamma/2)*Cov@gradf - (gamma/2)*Cov@subgradg
 
+def Drift(Z,phi,theta,A,c,lambda_R,lambda_O,barsh):
+
+    return 0
+    
 def PD3S(Z, phi, niter = 1e5):
     
     # lambda_O, lambda_R, c = phi?, nom du fichier?
@@ -92,10 +111,10 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
     barsh = Buildbarsh(T,a,b)
     c = np.array(phi)
     C = np.diag(c)
-    theta = np.concatenate((np.ones(T),np.zeros(T))) #Starting point
+    theta = np.concatenate((np.array(Z/phi), np.zeros(T))) #Starting point
     A = np.block([[D, np.zeros((T,T))],[np.zeros((T,T)), (lambda_O/lambda_R)*C]])
-    is_image = method in ["image", "subdiff_image"]
-    is_source = method in ["source", "subdiff_source"]
+    is_image = method in ["image"]
+    is_source = method in ["source"]
     
     # Covariance matrix Cov
     if is_image:
@@ -107,7 +126,7 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
         raise Exception("method must be either 'source' or 'image' ")
 
     # Burn-in aux variables
-    end_burn_in=None
+    end_burn_in=int(niter/2)
     burn_in = True
     wait_conv = False
     acceptance_cnt = 0
@@ -124,10 +143,109 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
     gammas.append(gamma)
     accepts.append(0)
     
-    plt.ion()
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-    ax1.set_title("Évolution de R")
-    ax2.set_title("Évolution de O")
+    #Computation of the MAP for comparison
+    MAPR,MAPO=np.split(MAP,2)
+
+    # burn-in loop
+    for i in range(int(niter/2)):
+
+        mu = theta
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )
+        
+        if log_alpha >=0 :
+            theta = candidate
+            acceptance_cnt += 1
+        else:
+            if rd[i] <= np.exp(log_alpha): # probability alpha of success
+                theta = candidate
+                acceptance_cnt += 1
+                
+        theta_tab[i+1,:]=theta   
+        
+        # burn-in
+        if ((i+1) % 1000) == 0:
+            accept_rate = acceptance_cnt / 1000
+            gamma += (accept_rate - accept_final) * gamma
+            gammas.append(gamma)
+            accepts.append(accept_rate)
+            acceptance_cnt = 0
+            if burn_in:
+                burn_in = abs(accept_rate - accept_final) > 1e-2
+                wait_conv = not burn_in
+            elif wait_conv:
+                converge += 1
+                wait_conv = converge < 2e-4 * niter
+                if not(wait_conv):
+                    end_burn_in=i
+                    break
+        if ((i+1) % 50000) == 0:
+            print(i)
+            
+    if(wait_conv):
+        end_burn_in=int(niter/2)
+    
+    print("End of the burn-in")
+
+    ## convergence loop
+    for i in range(end_burn_in,int(niter)):
+        mu = theta
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )
+        if log_alpha >=0 :
+            theta = candidate
+        else:
+            if rd[i] <= np.exp(log_alpha): # probability alpha of success
+                theta = candidate
+                
+        theta_tab[i+1,:]=theta 
+        if ((i+1) % 50000) == 0:
+            print(i)   
+            
+    theta_tilde_tab = np.einsum('ij,kj->ki', A, theta_tab)
+    return theta_tab,theta_tilde_tab, accepts, gammas,end_burn_in
+
+def MHSubdiff(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
+    
+    a = 0.73597
+    b = 0.73227
+    gamma = 0.001
+    accept_final = 0.24
+    D=BuildD(T)
+    barsh = Buildbarsh(T,a,b)
+    c = np.array(phi)
+    C = np.diag(c)
+    theta =np.concatenate((np.array(Z/phi), np.zeros(T))) #Starting point
+    A = np.block([[D, np.zeros((T,T))],[np.zeros((T,T)), (lambda_O/lambda_R)*C]])
+    is_image = method in ["image"]
+    is_source = method in ["source"]
+    
+    # Covariance matrix Cov
+    if is_image:
+        A_1 = npl.solve(A, np.identity(2*T))
+        Cov = A_1@A_1.T
+    elif is_source:
+        Cov = np.identity(2*T)
+    else:
+        raise Exception("method must be either 'source' or 'image' ")
+
+    # Burn-in aux variables
+    end_burn_in=int(niter/2)
+    burn_in = True
+    wait_conv = False
+    acceptance_cnt = 0
+    rd = npr.uniform(0, 1, int(niter+1))
+    theta_tab = np.empty((int(niter+1), 2*T))
+    theta_tab[0,:]=theta
+    theta_tilde_tab = np.empty((int(niter+1), 2*T))
+    converge=0
+
+    # for plotting
+
+    gammas = []
+    accepts = []
+    gammas.append(gamma)
+    accepts.append(0)
     
     #Computation of the MAP for comparison
     MAPR,MAPO=np.split(MAP,2)
@@ -135,7 +253,112 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
     # burn-in loop
     for i in range(int(niter/2)):
 
-        mu = theta
+        mu = CalculSubdiff(Z,phi,theta,A,c,lambda_R,lambda_O,barsh,gamma,Cov)
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-np.log(sps.multivariate_normal.pdf(candidate, mu, gamma*Cov))
+            
+        if log_alpha >=0 :
+            theta = candidate
+            acceptance_cnt += 1
+        else:
+            if rd[i] <= np.exp(log_alpha): # probability alpha of success
+                theta = candidate
+                acceptance_cnt += 1
+                
+        theta_tab[i+1,:]=theta   
+        
+        # burn-in
+        if ((i+1) % 1000) == 0:
+            accept_rate = acceptance_cnt / 1000
+            gamma += (accept_rate - accept_final) * gamma
+            gammas.append(gamma)
+            accepts.append(accept_rate)
+            acceptance_cnt = 0
+            if burn_in:
+                burn_in = abs(accept_rate - accept_final) > 1e-2
+                wait_conv = not burn_in
+            elif wait_conv:
+                converge += 1
+                wait_conv = converge < 2e-4 * niter
+                if not(wait_conv):
+                    end_burn_in=i
+                    break
+        if ((i+1) % 50000) == 0:
+            print(i)
+            
+    if(wait_conv):
+        end_burn_in=int(niter/2)
+    
+    print("End of the burn-in")
+
+    ## convergence loop
+    for i in range(end_burn_in,int(niter)):
+        mu = CalculSubdiff(Z,phi,theta,A,c,lambda_R,lambda_O,barsh,gamma,Cov)
+        candidate = npr.multivariate_normal(mu, gamma*Cov)
+        log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-np.log(sps.multivariate_normal.pdf(candidate, mu, gamma*Cov))+np.log(sps.multivariate_normal.pdf(theta, CalculSubdiff(Z,phi,candidate,A,c,lambda_R,lambda_O,barsh,gamma,Cov), gamma*Cov))
+
+        if log_alpha >=0 :
+            theta = candidate
+        else:
+            if rd[i] <= np.exp(log_alpha): # probability alpha of success
+                theta = candidate
+                
+        theta_tab[i+1,:]=theta 
+        if ((i+1) % 50000) == 0:
+            print(i)  
+            
+    theta_tilde_tab = np.einsum('ij,kj->ki', A, theta_tab)
+    return theta_tab,theta_tilde_tab, accepts, gammas,end_burn_in
+
+def MHProx(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
+    
+    a = 0.73597
+    b = 0.73227
+    gamma = 0.001
+    accept_final = 0.24
+    D=BuildD(T)
+    barsh = Buildbarsh(T,a,b)
+    c = np.array(phi)
+    C = np.diag(c)
+    theta = np.concatenate(Z/phi,np.zeros(T)) #Starting point
+    A = np.block([[D, np.zeros((T,T))],[np.zeros((T,T)), (lambda_O/lambda_R)*C]])
+    is_image = method in ["image"]
+    is_source = method in ["source"]
+    
+    # Covariance matrix Cov
+    if is_image:
+        A_1 = npl.solve(A, np.identity(2*T))
+        Cov = A_1@A_1.T
+    elif is_source:
+        Cov = np.identity(2*T)
+    else:
+        raise Exception("method must be either 'source' or 'image' ")
+
+    # Burn-in aux variables
+    end_burn_in=int(niter/2)
+    burn_in = True
+    wait_conv = False
+    acceptance_cnt = 0
+    rd = npr.uniform(0, 1, int(niter+1))
+    theta_tab = np.empty((int(niter+1), 2*T))
+    theta_tab[0,:]=theta
+    theta_tilde_tab = np.empty((int(niter+1), 2*T))
+    converge=0
+
+    # for plotting
+
+    gammas = []
+    accepts = []
+    gammas.append(gamma)
+    accepts.append(0)
+    
+    #Computation of the MAP for comparison
+    MAPR,MAPO=np.split(MAP,2)
+    
+    # burn-in loop
+    for i in range(int(niter/2)):
+
+        mu = Drift(Z,phi,theta,A,c,lambda_R,lambda_O,barsh)
         candidate = npr.multivariate_normal(mu, gamma*Cov)
         log_alpha = log_pi(candidate, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )-log_pi(theta, phi,Z, lambda_R, D, barsh, lambda_O,c ,C )
             
@@ -166,20 +389,7 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
                     end_burn_in=i
                     break
         if ((i+1) % 50000) == 0:
-            print(i+1)
-            R,O=np.split(theta,2)
-            ax1.clear()
-            ax2.clear()
-            ax1.plot(R,label=f"Simulation of R at iter {i}")
-            ax1.plot(MAPR,label="MAP R")
-            ax2.plot(O,label=f"Simulation of O at iter {i}")
-            ax2.plot(MAPO,label="MAP O")
-            ax1.set_title("Évolution de R")
-            ax2.set_title("Évolution de O")
-            ax1.legend()
-            ax2.legend()
-            plt.draw()
-            plt.pause(0.1)
+            print(i)
     if(wait_conv):
         end_burn_in=int(niter/2)
     
@@ -197,22 +407,8 @@ def MHRW(T, Z, phi, lambda_R,lambda_O,MAP,niter=1e5,method="source"):
                 theta = candidate
                 
         theta_tab[i+1,:]=theta 
-            
         if ((i+1) % 50000) == 0:
-            print(i+1)
-            R,O=np.split(theta,2)
-            ax1.clear()
-            ax2.clear()
-            ax1.plot(R,label=f"Simulation of R at iter {i}")
-            ax1.plot(MAPR,label="MAP R")
-            ax2.plot(O,label=f"Simulation of O at iter {i}")
-            ax2.plot(MAPO,label="MAP O")
-            ax1.legend()
-            ax2.legend()
-        
+            print(i)    
             
-    plt.ioff()
-    plt.show()
     theta_tilde_tab = np.einsum('ij,kj->ki', A, theta_tab)
     return theta_tab,theta_tilde_tab, accepts, gammas,end_burn_in
-
